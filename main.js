773ade65c75ef5c3d27f3d97992799e2aa008334
RiddhimaNaag2705 <86301596+RiddhimaNaag2705@users.noreply.github.com>
2021-07-25 14:58:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+    lipstick=loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
 }
 
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", getPoses);
 }
 
 function draw(){
-
+    image(video, 0, 0, 300,300);
+    image(lipstick, noseX, noseY, 50, 30);
 }
 
-function snap(){
+function snap(){ 
     save("LIPSTICK_Filter.png");
+}
+
+function modelLoaded(){
+    console.log("pose net loaded");
+}
+
+function getPoses(results){
+    if (results.length>0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x-25;
+        noseY=results[0].pose.nose.y+19;
+        
+    }
 }
